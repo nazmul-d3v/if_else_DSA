e9ab570006152a1eae61ef4ffb50277f567494d1
Nazmul Islam <124438941+nazmul-d3v@users.noreply.github.com>
2025-02-28 09:31:59 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int age;
+
+    cout<<"Enter the age:";
+    cin>> age;
+    if(age>=18)
+    {
+        cout<<" The parson is adult!";
+
+    }
+
+    else
+    {
+        cout<<"The parson is not adult!";
+
+    }
+
+    return 0;
+}
